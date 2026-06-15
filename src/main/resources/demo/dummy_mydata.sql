c6ad7090a4fb005537e5aa47b85933f9748713a3
LeeJuHyeong0492 <tkafkr100@naver.com>
2026-06-15 12:21:27 +0900

    [수정] 더미데이터 페르소나 수정

--- src/main/resources/demo/dummy_mydata.sql
+++ src/main/resources/demo/dummy_mydata.sql
@@ -7,11 +7,15 @@
 DELETE FROM dummy_mydata WHERE email = '{EMAIL}';
 
 INSERT INTO dummy_mydata (email, institution, asset_type, account_name, account_purpose, asset_number, balance, bank_type, is_salary) VALUES
-    ('{EMAIL}', '우리은행',   'CHECKING',        '우리 WON 통장',     null, '1002-111-111111',     6800000, 'WOORI', false),
-    ('{EMAIL}', '토스뱅크',   'PARKING',         '토스 파킹통장',     null, '4444-22-222222',      2000000, 'OTHER', false),
-    ('{EMAIL}', '카카오뱅크', 'SAVINGS',         '카뱅 26주 적금',    null,   '3333-33-333333',      1500000, 'OTHER', false),
-    ('{EMAIL}', '우리투자증권', 'STOCK',           '종합매매계좌',      null,  '5555-44-444444',      4000000, 'WOORI', false),
-    ('{EMAIL}', '토스뱅크',   'PARKING',         '여행 모음통장',     null,   '4444-66-666666',       335000, 'OTHER', false),
-    ('{EMAIL}', '한국투자증권',   'PENSION_SAVINGS',  '연금저축펀드',     null,   '8888-88-888888',      5000000, 'OTHER', false),
-    ('{EMAIL}', '우리카드',   'CREDIT_CARD',     '우리 카드의정석',   null,   '5570-1111-2222-3333',       0, 'WOORI', false),
-    ('{EMAIL}', '신한카드',   'CREDIT_CARD',     '신한 Deep Dream',   null,   '4000-4444-5555-6666',       0, 'OTHER', false);
+    ('{EMAIL}', '우리은행',     'CHECKING',    '우리 WON 통장',     null, '1002-111-111111',     6800000, 'WOORI', false),
+    ('{EMAIL}', '토스뱅크',     'CHECKING',    '토스뱅크 통장',     null, '4444-22-222222',      1200000, 'OTHER', false),
+    ('{EMAIL}', '카카오뱅크',   'CHECKING',    '카카오뱅크 통장',   null, '3333-33-333333',       850000, 'OTHER', false),
+    ('{EMAIL}', '국민은행',     'CHECKING',    'KB 국민 통장',      null, '6010-44-444444',      2100000, 'OTHER', false),
+    ('{EMAIL}', '신한은행',     'CHECKING',    '신한 SOL 통장',     null, '5550-55-555555',      1750000, 'OTHER', false),
+    ('{EMAIL}', '하나은행',     'CHECKING',    '하나 통장',         null, '6660-66-666666',       980000, 'OTHER', false),
+    ('{EMAIL}', '우리투자증권', 'ISA',         '우리 ISA 계좌',     null, '7700-11-111111',      3000000, 'WOORI', false),
+    ('{EMAIL}', '우리은행',     'SAVINGS',     '우리 정기적금',     null, '1002-222-222222',     1500000, 'WOORI', false),
+    ('{EMAIL}', '우리은행',     'DEPOSIT',     '우리 정기예금',     null, '1002-333-333333',     5000000, 'WOORI', false),
+    ('{EMAIL}', '우리투자증권', 'STOCK',       '종합매매계좌',      null, '5555-44-444444',      4000000, 'WOORI', false),
+    ('{EMAIL}', '우리카드',     'CREDIT_CARD', '우리 카드의정석',   null, '5570-1111-2222-3333',       0, 'WOORI', false),
+    ('{EMAIL}', '신한카드',     'CREDIT_CARD', '신한 Deep Dream',   null, '4000-4444-5555-6666',       0, 'OTHER', false);
